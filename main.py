fc37e4099a8bfb5bd5cea39bdc62916f5f7ccf63
Jordi <102949291+jblanchg@users.noreply.github.com>
2025-03-12 13:56:37 +0100

    feat: refactor to work properly with WMTS connections

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 """
-Copyright © 2024 by BGEO. All rights reserved.
+Copyright © 2025 by BGEO. All rights reserved.
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU
 General Public License as published by the Free Software Foundation, either version 3 of the License,
 or (at your option) any later version
 """
 from qgis.gui import QgisInterface
-from qgis.core import QgsRasterLayer, QgsProject, QgsLayerTreeLayer
+from qgis.core import QgsRasterLayer, QgsProject, QgsLayerTreeLayer, QgsMessageLog, Qgis
 from qgis.PyQt.QtCore import QSettings
 from qgis.PyQt.QtWidgets import QAction
 from qgis.PyQt.QtGui import QIcon
 from owslib.wmts import WebMapTileService
+import urllib
+import os
 
 
 class MapproxyTileManager:
     def __init__(self, iface: QgisInterface):
         self.iface = iface
+        self.plugin_dir = os.path.dirname(__file__)
 
     def initGui(self):
+
         # Create action that will start plugin configuration
         self.action = QAction(
-            QIcon("testplug:icon.png"),
+            QIcon(os.path.join(self.plugin_dir, 'icons/tile-manager.png')),
             "Mapproxy Tile Manager",
             self.iface.mainWindow()
         )
@@ -38,22 +42,39 @@ class MapproxyTileManager:
 
     def run(self):
         settings = QSettings()
-        # Found int QGIS .ini file
-        selected = settings.value("qgis/connections-WMS/selected")
-        capabilities_url = settings.value(f"qgis/connections-wms/{selected}/url")
-        username = settings.value(f"WMS/{selected}/username")
-        password = settings.value(f"WMS/{selected}/password")
+        # Check if the selected WMS connection is set
+        selected = settings.value("connections/ows/items/wms/connections/selected")
+        if not selected:
+            # If not set, set it to a default value ('mapproxy_server')
+            selected = 'mapproxy_server'
+            settings.setValue("ows/items/wms/connections/selected", selected)
+            QgsMessageLog.logMessage(f"Selected WMS connection was not set. Defaulting to: {selected}", level=Qgis.Critical)
 
-        print(f"Selected WMS: {selected}")
-        print(f"Capabilities URL: {capabilities_url}")
-        print(f"Username: {username}")
-        print(f"Password: {password}")
+        QgsMessageLog.logMessage(f"Using selected WMS connection: {selected}", level=Qgis.Info)
+
+        # Get connection details
+        capabilities_url_key = f"connections/ows/items/wms/connections/items/{selected}/url"
+        username_key = f"connections/ows/items/wms/connections/items/{selected}/username"
+        password_key = f"connections/ows/items/wms/connections/items/{selected}/password"
+
+        capabilities_url = settings.value(capabilities_url_key)
+        username = settings.value(username_key)
+        password = settings.value(password_key)
+
+        # Ensure all values are strings
+        capabilities_url = str(capabilities_url) if capabilities_url else None
+        username = str(username) if username else ""
+        password = str(password) if password else ""
+
+        if not capabilities_url:
+            QgsMessageLog.logMessage("Error: Capabilities URL is not set.", level=Qgis.Critical)
+            return
+
+        QgsMessageLog.logMessage(f"Selected WMS: {selected}", level=Qgis.Info)
+        QgsMessageLog.logMessage(f"Capabilities URL: {capabilities_url}", level=Qgis.Info)
 
         wmts = WebMapTileService(capabilities_url, username=username, password=password)
-        tile_matrix_name = next(iter(wmts.tilematrixsets.keys()))
-        tile_matrix = wmts.tilematrixsets[tile_matrix_name]
-        crs = tile_matrix.crs
-
+        matrix_iter = iter(wmts.tilematrixsets.keys())
         group_name = "Inventory"
 
         # Remove layers from the group if it already exists readd them
@@ -61,17 +82,27 @@ class MapproxyTileManager:
         group = root.findGroup(group_name)
         if group is None:
             group = root.addGroup(group_name)
-
         else:
             for child in group.children():
                 group.removeChildNode(child)
 
-        for layer_name in wmts.contents:
-            print(f"Layer: {layer_name}")
+        layers_to_add = []
 
-            # Create layer
+        for layer_name in wmts.contents:
+            QgsMessageLog.logMessage(f"Layer: {layer_name}", level=Qgis.Info)
+
+            tile_matrix_name = next(matrix_iter)
+            tile_matrix = wmts.tilematrixsets[tile_matrix_name]
+            crs = tile_matrix.crs
+
+            # Create layer with additional parameters
+            layer_source = (
+                f"IgnoreGetFeatureInfoUrl=1&IgnoreGetMapUrl=1&contextualWMSLegend=0&crs={crs}&dpiMode=7&format=image/png&layers={layer_name}"
+                f"&styles=default&tileMatrixSet={tile_matrix_name}&tilePixelRatio=0&url={urllib.parse.quote_plus(capabilities_url)}"
+            )
+
             wmts_layer = QgsRasterLayer(
-                f"crs={crs}&format=image/png&layers={layer_name}&styles=default&tileMatrixSet={tile_matrix_name}&url={capabilities_url}",
+                layer_source,
                 layer_name,
                 "wms"
             )
@@ -80,8 +111,17 @@ class MapproxyTileManager:
             wmts_layer.setDataUrl(f"wmts:{capabilities_url}#{layer_name}")
 
             if not wmts_layer.isValid():
-                print(f"Layer {layer_name} is not valid")
+                QgsMessageLog.logMessage(f"Layer {layer_name} is not valid", level=Qgis.Warning)
+                break;
+            # Append layer
+            layers_to_add.append(wmts_layer)
 
-            # Add layer to the group
-            QgsProject.instance().addMapLayer(wmts_layer, False)
-            group.insertChildNode(0, QgsLayerTreeLayer(wmts_layer))
+        # Add layers to the project
+        QgsProject.instance().addMapLayers(layers_to_add, False)
+
+        # Insert layers into the group
+        group.insertChildNodes(0,[QgsLayerTreeLayer(layer) for layer in layers_to_add])
+
+        # TODO: Uncheck visibility of the layers
+        # for layer in layers_to_add:
+        #     root.setItemVisibilityChecked(root.findLayer(layer.id()), False)
